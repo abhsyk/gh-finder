484afb2571dd43aa4705aad1ed33fdb6d8cf01c8
Sho Harada <tmchtmtr09876@gmail.com>
2023-02-16 18:55:05 +0900

    User list

---
+++ src/components/users/UserList/UserList.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect, useState } from 'react';
+import { User } from '../../../interfaces/User';
+
+const UserList: FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
+    });
+    const data = (await res.json()) as User[];
+
+    setUsers(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  return (
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users.map((user) => (
+        <h3 key={user.id}>{user.login}</h3>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
+import { UserList } from '../../components/users';
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <UserList />
+    </div>
+  );
 };
 
 export default Home;
