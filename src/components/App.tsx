eb621892c918d549e8601f6f5c36e43f81cb73e3
Sho Harada <tmchtmtr09876@gmail.com>
2023-02-16 18:16:07 +0900

    Add pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import About from '../pages/About';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import { Layout } from './layout';
 
 function App() {
   return (
     <Layout>
-      <div>
-        <h1 className="text-xl">Hello</h1>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Layout>
   );
 }
